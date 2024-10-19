ce2a90588974cc3a1a8bb70550b375508f97e53f
Justin Simon <jtsimon@jtsimon.dev>
2024-10-19 16:11:34 -0400

    Create INeedsService.cs

---
+++ BlueRidgeRelief.Core/Interfaces/INeedsService.cs
@@ -0,0 +1,17 @@
+﻿using BlueRidgeRelief.Core.Entities;
+
+namespace BlueRidgeRelief.Core.Interfaces;
+
+public interface INeedsService
+{
+    Task<IEnumerable<Need>> GetNeedsAsync();
+    Task<Need?> GetNeedByIdAsync(int id);
+    Task<Need> CreateNeedAsync(Need need);
+    Task<Need> UpdateNeedAsync(Need need);
+    Task<IEnumerable<Need>> GetNeedsByUserIdAsync(string userId);
+    Task<IEnumerable<Need>> GetNeedsByCategoryAsync(string category);
+    Task<IEnumerable<Need>> GetNeedsByUrgencyLevelAsync(string urgencyLevel);
+    Task<IEnumerable<Need>> GetNeedsByStatusAsync(string status);
+    Task<IEnumerable<Need>> GetNeedsByLocationAsync(string location);
+    Task<IEnumerable<Need>> GetNeedsByLocationAsync(double latitude, double longitude, double radius); 
+}
