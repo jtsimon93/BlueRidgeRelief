3892b5fe4c1dec86a1cc6c2803990b1f7758b564
Justin Simon <jtsimon@jtsimon.dev>
2024-10-20 12:28:40 -0400

    Create custom user detail & needs services

---
+++ BlueRidgeRelief.Services/CustomUserDetailsService.cs
@@ -0,0 +1,30 @@
+﻿using BlueRidgeRelief.Core.Entities;
+using BlueRidgeRelief.Core.Interfaces;
+namespace BlueRidgeRelief.Services;
+
+
+public class CustomUserDetailsService : ICustomUserDetailsService
+{
+   private readonly ICustomUserDetailsRepository _customUserDetailsRepository;
+
+   public CustomUserDetailsService (ICustomUserDetailsRepository customUserDetailsRepository)
+   {
+       _customUserDetailsRepository = customUserDetailsRepository;
+   }
+   
+   public Task<CustomUserDetails?> GetCustomUserDetailsByUserIdAsync(string userId)
+   {
+       return _customUserDetailsRepository.GetCustomUserDetailsByUserIdAsync(userId);
+   }
+   
+   public Task<CustomUserDetails> CreateCustomUserDetailsAsync(CustomUserDetails customUserDetails)
+   {
+       return _customUserDetailsRepository.CreateCustomUserDetailsAsync(customUserDetails);
+   }
+   
+   public Task<CustomUserDetails> UpdateCustomUserDetailsAsync(CustomUserDetails customUserDetails)
+   {
+       return _customUserDetailsRepository.UpdateCustomUserDetailsAsync(customUserDetails);
+   }
+   
+}
---
+++ BlueRidgeRelief.Services/NeedsService.cs
@@ -0,0 +1,65 @@
+﻿using BlueRidgeRelief.Core.Entities;
+using BlueRidgeRelief.Core.Interfaces;
+
+namespace BlueRidgeRelief.Services;
+
+public class NeedsService : INeedsService
+{
+    private readonly INeedsRepository _needsRepository;
+    
+    public NeedsService(INeedsRepository needsRepository)
+    {
+        _needsRepository = needsRepository;
+    }
+    
+    public Task<IEnumerable<Need>> GetNeedsAsync()
+    {
+        return _needsRepository.GetNeedsAsync();
+    }
+    
+    public Task<Need?> GetNeedByIdAsync(int id)
+    {
+        return _needsRepository.GetNeedByIdAsync(id);
+    }
+    
+    public Task<Need> CreateNeedAsync(Need need)
+    {
+        return _needsRepository.CreateNeedAsync(need);
+    }
+    
+    public Task<Need> UpdateNeedAsync(Need need)
+    {
+        return _needsRepository.UpdateNeedAsync(need);
+    }
+    
+    public Task<IEnumerable<Need>> GetNeedsByUserIdAsync(string userId)
+    {
+        return _needsRepository.GetNeedsByUserIdAsync(userId);
+    }
+    
+    public Task<IEnumerable<Need>> GetNeedsByCategoryAsync(string category)
+    {
+        return _needsRepository.GetNeedsByCategoryAsync(category);
+    }
+    
+    public Task<IEnumerable<Need>> GetNeedsByUrgencyLevelAsync(string urgencyLevel)
+    {
+        return _needsRepository.GetNeedsByUrgencyLevelAsync(urgencyLevel);
+    }
+    
+    public Task<IEnumerable<Need>> GetNeedsByStatusAsync(string status)
+    {
+        return _needsRepository.GetNeedsByStatusAsync(status);
+    }
+    
+    public Task<IEnumerable<Need>> GetNeedsByLocationAsync(string location)
+    {
+        return _needsRepository.GetNeedsByLocationAsync(location);
+    }
+    
+    public Task<IEnumerable<Need>> GetNeedsByLocationAsync(double latitude, double longitude, double radius)
+    {
+        return _needsRepository.GetNeedsByLocationAsync(latitude, longitude, radius);
+    }
+    
+}
